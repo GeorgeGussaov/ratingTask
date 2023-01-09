c3b318a4afaf0b47f8a0e1e791d5e2190e88b26a
Kasumyan2004@outlook.com <Kasumyan2004@outlook.com>
2023-01-09 11:57:11 +0300

    Добавьте файлы проекта.

---
+++ ratingTask/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ratingTask
+{
+    partial class FormMonthConverter
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMonthNumber = new System.Windows.Forms.TextBox();
+            this.textBoxMonthName = new System.Windows.Forms.TextBox();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.listBoxInMonthDaysCnt = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(58, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Номер месяца:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label2.Location = new System.Drawing.Point(250, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Месяц:";
+            // 
+            // textBoxMonthNumber
+            // 
+            this.textBoxMonthNumber.Location = new System.Drawing.Point(61, 132);
+            this.textBoxMonthNumber.Name = "textBoxMonthNumber";
+            this.textBoxMonthNumber.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMonthNumber.TabIndex = 2;
+            // 
+            // textBoxMonthName
+            // 
+            this.textBoxMonthName.Location = new System.Drawing.Point(253, 132);
+            this.textBoxMonthName.Name = "textBoxMonthName";
+            this.textBoxMonthName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMonthName.TabIndex = 3;
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(165, 183);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(93, 23);
+            this.buttonOk.TabIndex = 5;
+            this.buttonOk.Text = "Активировать";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // listBoxInMonthDaysCnt
+            // 
+            this.listBoxInMonthDaysCnt.FormattingEnabled = true;
+            this.listBoxInMonthDaysCnt.Location = new System.Drawing.Point(61, 262);
+            this.listBoxInMonthDaysCnt.Name = "listBoxInMonthDaysCnt";
+            this.listBoxInMonthDaysCnt.Size = new System.Drawing.Size(289, 95);
+            this.listBoxInMonthDaysCnt.TabIndex = 7;
+            // 
+            // FormMonthConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 450);
+            this.Controls.Add(this.listBoxInMonthDaysCnt);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.textBoxMonthName);
+            this.Controls.Add(this.textBoxMonthNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMonthConverter";
+            this.Text = "MonthConverter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMonthNumber;
+        private System.Windows.Forms.TextBox textBoxMonthName;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.ListBox listBoxInMonthDaysCnt;
+    }
+}
+
---
+++ ratingTask/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ratingTask
+{
+    public partial class FormMonthConverter : Form
+    {
+        public FormMonthConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            string monthNum = textBoxMonthNumber.Text;
+            int intMonthNum = Convert.ToInt32(monthNum);
+            if (intMonthNum > 12 || intMonthNum < 1)
+            {
+                listBoxInMonthDaysCnt.Items.Add("Выход за границы массива.");
+                return;
+            }
+            int cntDays = 0;
+            if (buttonOk.Enabled == true)
+            {
+                switch (monthNum)
+                {
+                    case "1": textBoxMonthName.Text = "Январь"; break;
+                    case "2": textBoxMonthName.Text = "Февраль"; break;
+                    case "3": textBoxMonthName.Text = "Март"; break;
+                    case "4": textBoxMonthName.Text = "Апрель"; break;
+                    case "5": textBoxMonthName.Text = "Май"; break;
+                    case "6": textBoxMonthName.Text = "Июнь"; break;
+                    case "7": textBoxMonthName.Text = "Июль"; break;
+                    case "8": textBoxMonthName.Text = "Август"; break;
+                    case "9": textBoxMonthName.Text = "Сентябрь"; break;
+                    case "10": textBoxMonthName.Text = "Октябрь"; break;
+                    case "11": textBoxMonthName.Text = "Ноябрь"; break;
+                    case "12": textBoxMonthName.Text = "Декабрь"; break;
+                }
+                switch (monthNum)
+                {
+                    case "1":cntDays = 31; break;
+                    case "2": cntDays = 28; break;
+                    case "3": cntDays = 31; break;
+                    case "4": cntDays = 30; break;
+                    case "5": cntDays = 31; break;
+                    case "6": cntDays = 30; break;
+                    case "7": cntDays = 31; break;
+                    case "8": cntDays = 31; break;
+                    case "9": cntDays = 30; break;
+                    case "10": cntDays = 31; break;
+                    case "11": cntDays = 30; break;
+                    case "12": cntDays = 31; break;
+                }
+                listBoxInMonthDaysCnt.Items.Add(textBoxMonthName.Text + " - " + cntDays);
+            }
+        }
+    }
+}
